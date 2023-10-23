092da66b11cae889a007d42561225b6c49a7f010
ShahnamFeyzian <shahnamecfman@gmail.com>
2023-10-23 15:15:34 +0330

    sahel

--- ARM/Code/Testbench.v
+++ ARM/Code/Testbench.v
@@ -1,6 +1,8 @@
+`timescale 1ns/1ns
+
 module Testbench();
     reg clk, rst;
-    ARM arm(.CLK_50(clk), .SW({17{0}, rst}));
+    ARM arm(.CLK_50(clk), .SW({17'b0, rst}));
 
     always #5 clk = ~clk;
 
@@ -8,6 +10,7 @@ module Testbench();
         rst = 0;
         #30 rst = 1;
         #30 rst = 0;
+        #200 $stop;
     end
 
 endmodule
