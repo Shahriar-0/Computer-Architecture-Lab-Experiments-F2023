53b78f888a5546bfe166d4af91125cff87769eff
Shahriar-0 <attarshahriar@gmail.com>
2023-11-06 12:38:03 +0330

    reg 0 regfile

--- ARM/Code/RegFile.v
+++ ARM/Code/RegFile.v
@@ -14,10 +14,13 @@ module RegisterFile(clk, regWrite,
     
     output [WordLen-1:0] readData1, readData2;
 
-    reg [WordLen-1:0] registerFile [0:WordCount-2];
+    reg [WordLen-1:0] registerFile [0:WordCount-1];
+
+    initial 
+        registerFile[0] = 32'd0;
 
     always @(negedge clk) begin
-        if (regWrite)
+        if (regWrite & (|writeRegister))
             registerFile[writeRegister] <= writeData;
     end
 
