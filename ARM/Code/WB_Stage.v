4cd43fcd88a9c7b387581e4f5107ddd8311f29f0
Shahriar-0 <attarshahriar@gmail.com>
2023-11-13 14:08:21 +0330

    wb

--- ARM/Code/WB_Stage.v
+++ ARM/Code/WB_Stage.v
@@ -1,7 +1,13 @@
 
-module WB_Stage(clk, rst);
+module WB_Stage(clk, rst, ALU_ResIn, DataMemoryIn, MEM_R_ENIn, WB_DestIn, WB_DestOut, WB_ENIn, WB_ENOut, WB_ValueOut, WB_ValueOut);
 
     parameter N = 32;
     input wire[0:0] clk, rst;
+
+    Mux2to1 mux(.a(ALU_ResIn), .b(DataMemoryIn), .s(MEM_R_ENIn), .out(WB_ValueOut));
+
+    assign WB_ENOut = WB_ENIn;
+    assign WB_DestOut = WB_DestIn;
+
 
 endmodule
