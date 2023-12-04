eaf7c36161ff11a8d5e09981133fba53fa3ea24f
Shahriar-0 <attarshahriar@gmail.com>
2023-12-04 11:26:50 +0330

    meow

--- ARM/Code/ForwardingUnit.v
+++ ARM/Code/ForwardingUnit.v
@@ -11,25 +11,21 @@ module ForwardingUnit(forwardEnIn, src1In, src2In,
     always @(*) begin
         selSrc1Out = 2'b00;
         if (forwardEnIn) begin
-            if (MEM_MEMR_WB_ENIn && (MEM_MEMR_DestIn == src1In)) begin
+            if (MEM_MEMR_WB_ENIn && (MEM_MEMR_DestIn == src1In)) 
                 selSrc1Out = 2'b01;
-            end
-            else if (WB_ID_WB_ENIn && (WB_ID_DestIn == src1In)) begin
+            else if (WB_ID_WB_ENIn && (WB_ID_DestIn == src1In)) 
                 selSrc1Out = 2'b10;
-            end
         end
     end
 
     always @(*) begin
         selSrc2Out = 2'b00;
         if (forwardEnIn) begin
-            if (MEM_MEMR_WB_ENIn && (MEM_MEMR_DestIn == src2In)) begin
+            if (MEM_MEMR_WB_ENIn && (MEM_MEMR_DestIn == src2In))
                 selSrc2Out = 2'b01;
-            end
-            else if (WB_ID_WB_ENIn && (WB_ID_DestIn == src2In)) begin
+            else if (WB_ID_WB_ENIn && (WB_ID_DestIn == src2In))
                 selSrc2Out = 2'b10;
-            end
         end
     end
-    
+
 endmodule
